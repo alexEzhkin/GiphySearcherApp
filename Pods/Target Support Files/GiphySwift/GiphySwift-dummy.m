22e191e96d7725ac41ade209550ba82b82148121
Alex <>
2018-08-15 23:17:05 +0300

    Commit

--- Pods/Target Support Files/GiphySwift/GiphySwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GiphySwift : NSObject
-@end
-@implementation PodsDummy_GiphySwift
-@end
--- Pods/Target Support Files/Pods-GiphySearcher/Pods-GiphySearcher-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GiphySearcher : NSObject
-@end
-@implementation PodsDummy_Pods_GiphySearcher
-@end
